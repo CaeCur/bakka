c91a372603ec9123e26b30413932a1f95d7570ba
Caelan <caelan.curry@gmail.com>
2022-08-22 17:13:23 +0200

    Started implementation of video details page

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,7 +1,80 @@
-import React from "react";
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import { BASE_URL } from "../../utils/index";
 
-const Detail = () => {
-  return <div>Video Details Page</div>;
+// icons import
+import { GoVerified } from "react-icons/go";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsFillPlayFill } from "react-icons/bs";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+
+//types
+import { Video } from "../../types";
+
+interface IProps {
+  postDetails: Video;
+}
+
+export const getServerSideProps = async ({ params: { id } }: { params: { id: string } }) => {
+  const { data } = await axios.get(`${BASE_URL}/api/post/${id}`);
+
+  return {
+    props: { postDetails: data },
+  };
+};
+
+const Detail = ({ postDetails }: IProps) => {
+  const [post, setPost] = useState(postDetails);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isMuted, setIsMuted] = useState(true);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const onVideoClick = () => {
+    if (isPlaying) {
+      videoRef?.current?.pause();
+      setIsPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setIsPlaying(true);
+    }
+  };
+
+  if (!post) return null; //This should only happen if user manually inputs incorrect post id
+
+  return (
+    <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
+      <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center">
+        <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
+          <p>
+            <MdOutlineCancel className="text-white text-[35px]" />
+          </p>
+        </div>
+        <div className="relative">
+          <div className="lg:h-[100vh] h-[60vh]">
+            <video
+              ref={videoRef}
+              loop
+              onClick={onVideoClick}
+              src={post.video.asset.url}
+              className="h-full cursor-pointer"
+            ></video>
+          </div>
+
+          <div className="absolute top-[45%] left-[45%] cursor-pointer">
+            {!isPlaying && (
+              <button onClick={onVideoClick}>
+                <BsFillPlayFill className="text-white text-6xl lg:text-8xl" />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
