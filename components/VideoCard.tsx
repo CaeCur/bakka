e3a513b934c7d4b3339ab484e183040d65e7c203
Caelan <caelan.curry@gmail.com>
2022-08-22 15:38:26 +0200

    Centre app layout

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -75,7 +75,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
               ref={videoRef}
               src={post.video.asset.url}
               loop
-              className="lg:w-[90%] h-[300px] md:h-[400px] lg:h-[530px] w-[70%] rounded-2xl cursor-pointer bg-gray-100"
+              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
             ></video>
           </Link>
 
