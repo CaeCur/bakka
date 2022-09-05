93ef2cdce76858ffd0aa95c7596ba2324959adbe
Caelan <caelan.curry@gmail.com>
2022-09-05 12:05:25 +0200

    Added search functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { createOrGetUser } from "../utils";
 import useAuthStore from "../store/authStore";
 import Image from "next/image";
@@ -10,7 +11,18 @@ import { IoMdAdd } from "react-icons/io";
 import Logo from "../utils/bakka-logo.png";
 
 const Navbar = () => {
+  const [searchValue, setSearchValue] = useState("");
+
   const { userProfile, addUser, removeUser } = useAuthStore();
+  const router = useRouter();
+
+  const handleSearch = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    if (searchValue) {
+      router.push(`/search/${searchValue}`);
+    }
+  };
 
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
@@ -19,7 +31,26 @@ const Navbar = () => {
           <Image className="cursor-pointer" src={Logo} alt="Bakka" layout="responsive" priority />
         </div>
       </Link>
-      <div>SEARCH</div>
+
+      <div className="relative hidden md:block">
+        <form onSubmit={handleSearch} className="absolute md:static top-10 -left-20 bg-white">
+          <input
+            value={searchValue}
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
+            placeholder="Search..."
+            className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full  md:top-0"
+          />
+          <button
+            onClick={handleSearch}
+            className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
+          >
+            <BiSearch />
+          </button>
+        </form>
+      </div>
+
       <div>
         {userProfile ? (
           <div className="flex gap-5 md:gap-10">
