bb7075d4c091490e0d0eb4a86ae1653b67ff25f5
Caelan <caelan.curry@gmail.com>
2022-09-05 12:19:46 +0200

    Add topic based search

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,12 +11,18 @@ interface IProps {
   videos: Video[];
 }
 
-export const getServerSideProps = async () => {
-  const { data } = await axios.get(`${BASE_URL}/api/post`);
+export const getServerSideProps = async ({ query: { topic } }: { query: { topic: string } }) => {
+  let response = null;
+
+  if (topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  } else {
+    response = await axios.get(`${BASE_URL}/api/post`);
+  }
 
   return {
     props: {
-      videos: data,
+      videos: response.data,
     },
   };
 };
